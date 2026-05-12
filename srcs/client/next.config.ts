c464fc289112966a3e99ea3d8d6d07ee90b91f74
Chiaki-Sakamoto-work <chiakisakamoto.work@gmail.com>
2026-05-12 16:17:24 +0900

    refactor(lint): change double quotation to single quotation 󰑌

--- srcs/client/next.config.ts
+++ srcs/client/next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // TurbopackやWebpackの解析対象から lightningcss を除外する
-  serverExternalPackages: ["lightningcss"],
+  serverExternalPackages: ['lightningcss'],
 };
 
 export default nextConfig;
