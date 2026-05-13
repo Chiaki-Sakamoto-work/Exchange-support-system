0c031f807d63e6056fee24607d9cc07656ed4038
pisyaru <jinyeong1114@gmail.com>
2026-05-13 13:37:49 +0900

    feat: add card varient shadow-none and destructive

--- srcs/client/components/ui/Card.tsx
+++ srcs/client/components/ui/Card.tsx
@@ -5,19 +5,32 @@ import { cn } from '@/lib/utils';
 function Card({
   className,
   size = 'default',
+  variant = 'default',
   ...props
-}: React.ComponentProps<'div'> & { size?: 'default' | 'sm' | 'xs' }) {
+}: React.ComponentProps<'div'> & {
+  size?: 'default' | 'sm' | 'xs';
+  variant?: 'default' | 'shadow-none' | 'destructive';
+}) {
   return (
     <div
       data-slot='card'
       data-size={size}
+      data-variant={variant}
       className={cn(
-        'group/card flex flex-col overflow-hidden rounded-[16px] bg-card text-sm text-card-foreground',
-        'ring-1 ring-inset ring-card-foreground/[8%]',
-        'shadow-[0px_5.89px_17.66px] shadow-card-foreground/[8%]',
+        'group/card flex flex-col overflow-hidden rounded-[16px] text-sm',
+
+        'data-[variant=default]:bg-card data-[variant=default]:text-card-foreground',
+        'data-[variant=default]:ring-1 data-[variant=default]:ring-inset data-[variant=default]:ring-card-foreground/[8%]',
+        'data-[variant=default]:shadow-[0px_5.89px_17.66px] data-[variant=default]:shadow-card-foreground/[8%]',
+
+        'data-[variant=shadow-none]:bg-card data-[variant=shadow-none]:text-card-foreground',
+
+        'data-[variant=destructive]:bg-destructive/10 data-[variant=destructive]:text-destructive',
+        'data-[variant=destructive]:ring-1 data-[variant=destructive]:ring-inset data-[variant=destructive]:ring-destructive/20',
+
         'data-[size=default]:w-158 data-[size=default]:h-36.5 data-[size=default]:gap-4 data-[size=default]:py-4',
         'data-[size=sm]:w-91 data-[size=sm]:h-11 data-[size=sm]:gap-3 data-[size=sm]:py-3',
-        'data-[size=xs]:w-20 data-[size=xs]:h-6 data-[size=sm]:gap-2 data-[size=sm]:py-2',
+        'data-[size=xs]:w-20 data-[size=xs]:h-6 data-[size=sm]:gap-2 data-[size=xs]:py-2',
         'has-data-[slot=card-footer]:pb-0 has-[>img:first-child]:pt-0 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl',
         className,
       )}
@@ -32,6 +45,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
       data-slot='card-header'
       className={cn(
         'flex flex-row items-center group/card-header @container/card-header auto-rows-min gap-1 rounded-t-xl px-4 group-data-[size=sm]/card:px-3 has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto] [.border-b]:pb-4 group-data-[size=sm]/card:[.border-b]:pb-3 ',
+        'group-data-[variant=destructive]/card:text-destructive',
         '[&>[data-slot=card-action]]:ml-auto',
         className,
       )}
@@ -46,6 +60,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<'div'>) {
       data-slot='card-title'
       className={cn(
         'text-base leading-snug font-medium group-data-[size=sm]/card:text-sm',
+        'group-data-[variant=destructive]/card:text-destructive',
         className,
       )}
       {...props}
@@ -82,6 +97,9 @@ function CardContent({ className, ...props }: React.ComponentProps<'div'>) {
       data-slot='card-content'
       className={cn(
         'px-4 group-data-[size=sm]/card:px-3 text-sm text-muted-foreground flex-1 flex flex-col',
+
+        'group-data-[variant=destructive]/card:text-destructive',
+
         'group-data-[size=sm]/card:px-3 group-data-[size=sm]/card:justify-center',
         'group-data-[size=xs]/card:px-2 group-data-[size=xs]/card:justify-center group-data-[size=xs]/card:items-center group-data-[size=xs]/card:text-center',
         className,
