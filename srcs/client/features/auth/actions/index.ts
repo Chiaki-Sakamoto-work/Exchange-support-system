e4d73b93b0c2649db7659d11e6a8dadd2d9fa274
Chiaki-Sakamoto-work <chiakisakamoto.work@gmail.com>
2026-05-14 15:23:48 +0900

    refactor(rebase): rebase develop branch 󰑌

--- srcs/client/features/auth/actions/index.ts
+++ srcs/client/features/auth/actions/index.ts
@@ -3,13 +3,18 @@
 import { redirect } from 'next/navigation';
 import { createClient } from '@/lib/supabase/server';
 
+export async function signOut() {
+  const supabase = await createClient();
+  await supabase.auth.signOut();
+  redirect('/login');
+}
 export async function signInWithGoogle() {
   const supabase = await createClient();
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${process.env.AUTH_GOOGLE_CALLBACK_URL}`,
+      redirectTo: process.env.AUTH_GOOGLE_CALLBACK_URL,
     },
   });
 
@@ -19,12 +24,15 @@ export async function signInWithGoogle() {
   }
 
   if (data.url) {
-    redirect(data.url); // Googleのログイン画面へ飛ばす
-  }
-}
+    let browserUrl = data.url;
 
-export async function signOut() {
-  const supabase = await createClient();
-  await supabase.auth.signOut();
-  redirect('/login');
+if (process.env.NODE_ENV === 'development') {
+      browserUrl = data.url.replace('host.docker.internal', 'localhost');
+    }
+    
+    console.log('🔗 Redirecting browser to:', browserUrl);
+    
+    // 書き換えたURLでリダイレクト
+    return redirect(browserUrl);
+  }
 }
