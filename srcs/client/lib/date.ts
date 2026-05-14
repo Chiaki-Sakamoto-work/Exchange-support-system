02769164ac3c198cfe6e1211567e5f998a75f71d
pisyaru <jinyeong1114@gmail.com>
2026-05-14 17:50:17 +0900

    fix: change format of formatdate

--- srcs/client/lib/date.ts
+++ srcs/client/lib/date.ts
@@ -1,10 +1,14 @@
-// 日付を見やすく変換する関数 (2026-05-20T... -> 5/20 19:00)
 export const formatDate = (date: Date | null) => {
   if (!date) return '日時未定';
-  return new Date(date).toLocaleString('ja-JP', {
-    month: 'numeric',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-  });
+
+  const d = new Date(date);
+
+  const yyyy = d.getFullYear();
+
+  const mm = String(d.getMonth() + 1).padStart(2, '0');
+  const dd = String(d.getDate()).padStart(2, '0');
+  const hh = String(d.getHours()).padStart(2, '0');
+  const min = String(d.getMinutes()).padStart(2, '0');
+
+  return `${yyyy}-${mm}-${dd} ${hh}:${min}`;
 };
