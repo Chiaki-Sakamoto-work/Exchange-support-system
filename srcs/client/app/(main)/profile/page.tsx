69b65bd1e94199c49bca131e3d55eeb9553bc26a
Chiaki-Sakamoto-work <chiakisakamoto.work@gmail.com>
2026-05-15 17:53:35 +0900

    feat(implementation profile): implementation profile function 

--- srcs/client/app/(main)/profile/page.tsx
+++ srcs/client/app/(main)/profile/page.tsx
@@ -1,19 +1,17 @@
-import { ProfileView } from '@/features/profile/components/ProfileView';
+import { ProfileDashboard } from '@/features/profile/components/ProfileDashboard';
 import { prisma } from '@/lib/prisma';
 import { auth } from '@/lib/supabase/auth';
 
 export default async function ProfilePage() {
   const session = await auth();
   
-  // DBから最新のユーザー情報を取得（Cでいうところの SELECT * FROM profiles WHERE id = ...）
   const profile = await prisma.profiles.findUnique({
     where: { id: session.user.id },
   });
 
   return (
     <main className="p-4">
-      {/* 取得した生データを View に流し込む */}
-      <ProfileView user={profile} />
+      <ProfileDashboard user={profile} />
     </main>
   );
 }
