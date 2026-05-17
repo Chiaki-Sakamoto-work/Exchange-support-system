3901b8f399bcc4654410370d4109fd1865d0dd67
Chiaki-Sakamoto-work <chiakisakamoto.work@gmail.com>
2026-05-18 07:22:44 +0900

    wip

--- srcs/client/features/auth/actions/index.ts
+++ srcs/client/features/auth/actions/index.ts
@@ -14,7 +14,7 @@ export async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: process.env.AUTH_GOOGLE_CALLBACK_URL,
+      redirectTo: `${process.env.FRONTEND}/callback`,
     },
   });
 
