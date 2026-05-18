5a69d703189b27409f40cb25caf290c8b0f7d3c8
Chiaki-Sakamoto-work <chiakisakamoto.work@gmail.com>
2026-05-18 12:22:08 +0900

    fix(fix auth): fix google auth 

--- Makefile
+++ Makefile
@@ -45,6 +45,9 @@ db-migrate:
 	cd ${SRCDIR} && docker compose exec app npx prisma migrate dev
 	# schema.prisma の変更をローカルDBに反映する（マイグレーション）
 
+db-reset:
+	cd ${SRCDIR} && docker compose exec app npx prisma migrate reset
+
 ##------ お掃除 ------##
 # Next.jsもSupabaseも全部落として、コンテナを綺麗にする
 clean: down db-down
--- srcs/client/features/profile/actions/profile.ts
+++ srcs/client/features/profile/actions/profile.ts
@@ -4,27 +4,47 @@ import { revalidatePath } from 'next/cache';
 import { prisma } from '@/lib/prisma';
 import { type ProfileFormValues, profileSchema } from '../schemas/profile';
 
-export async function updateProfile(userId: string, data: ProfileFormValues) {
+// 💡 引数に email と avatarUrl を追加します
+export async function updateProfile(
+  userId: string,
+  email: string,
+  avatarUrl: string | null,
+  data: ProfileFormValues,
+) {
   const validatedData = profileSchema.parse(data);
 
   try {
-    await prisma.profiles.update({
+    await prisma.profiles.upsert({
       where: { id: userId },
-      data: {
+      // 💡 既にレコードがある場合（Update）
+      update: {
         username: validatedData.username,
         bio: data.bio,
         department_id: validatedData.department_id,
         user_type: validatedData.user_type,
         allergies: validatedData.allergies,
         is_support_used: validatedData.is_support_used,
+        avatar_url: avatarUrl, // 💡 Google側でアイコンが変わった場合のために、アップデート時も上書きする
         updated_at: new Date(),
+      },
+      // 💡 レコードが無い場合：db-reset直後など（Create）
+      create: {
+        id: userId, // Supabaseから取ってきた sub (ユーザーID)
+        email: email, // Supabaseから取ってきた email
+        avatar_url: avatarUrl, // Supabaseから取ってきた avatar_url
+        username: validatedData.username,
+        bio: data.bio,
+        department_id: validatedData.department_id,
+        user_type: validatedData.user_type,
+        allergies: validatedData.allergies,
+        is_support_used: validatedData.is_support_used,
       },
     });
 
     revalidatePath('/profile');
     return { success: true };
   } catch (error) {
-    console.error(error);
+    console.error('Prisma Upsert Error:', error);
     return { success: false, error: '更新に失敗しました' };
   }
 }
