ffa7fe164782d3152c30b70356e969325c03b9d3
Chiaki-Sakamoto-work <chiakisakamoto.work@gmail.com>
2026-05-19 18:42:52 +0900

    wip: implement chat

--- srcs/client/features/chat/actions/chat.ts
+++ srcs/client/features/chat/actions/chat.ts
@@ -1,3 +1,24 @@
 'use server';
 
 import { prisma } from '@/lib/prisma';
+import { fullMessageInclude } from '@type';
+
+const getMessages = async (roomId: number) => {
+  try {
+    const message = await prisma.message.findMany({
+      where: {
+        room_id: roomId
+      },
+      include: fullMessageInclude,
+      orderBy: {
+        created_at: 'asc'
+      },
+    });
+
+    return message;
+  } catch (error) {
+    console.error('メッセージ取得エラー:', error);
+    throw new Error('メッセージ取得に失敗しました');
+  }
+};
+
---
+++ srcs/client/types/message.ts
@@ -0,0 +1,12 @@
+import type { Prisma } from '@prisma/client';
+
+const fullMessageInclude = {
+  profiles: true, 
+} satisfies Prisma.messagesInclude;
+
+type ChatMessage = Prisma.messagesGetPayload<{
+  include: typeof fullMessageInclude;
+}>;
+
+export { fullMessageInclude, type ChatMessage };
+
