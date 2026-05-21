37f173d86bfbe7d374c2b8808da1aefc8eb0d6a9
hopanglee <hgknim87j@naver.com>
2026-05-22 00:56:52 +0900

    fix/add loading.tsx for bottom nav error when page moving

---
+++ srcs/client/app/(main)/profile/edit/loading.tsx
@@ -0,0 +1,5 @@
+import { ProfileEditLoadingSkeleton } from '@/features/profile/components/ProfileLoadingSkeleton';
+
+export default function Loading() {
+  return <ProfileEditLoadingSkeleton />;
+}
