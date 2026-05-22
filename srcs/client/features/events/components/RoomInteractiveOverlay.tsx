9ad473b816465877d92bea3189e69dd47ab12a0b
deco-L <scotchwitchbroom@gmail.com>
2026-05-22 11:45:27 +0900

    fix(chat): chat function prohibit 

--- srcs/client/features/events/components/RoomInteractiveOverlay.tsx
+++ srcs/client/features/events/components/RoomInteractiveOverlay.tsx
@@ -1,6 +1,9 @@
 'use client';
 
-import { ChatPanel } from '@feature/chat/components/ChatPanel';
+{
+  /*import { ChatPanel } from '@feature/chat/components/ChatPanel';*/
+}
+
 import { EventInfoPanel } from '@feature/events/components/EventInfoPanel';
 import { useState } from 'react';
 
