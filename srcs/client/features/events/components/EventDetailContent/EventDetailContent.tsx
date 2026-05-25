bb6d726af9fb36903478d01f6fe720535c755d22
deco-L <scotchwitchbroom@gmail.com>
2026-05-25 12:40:59 +0900

    refactor(dialogBody): dialogBody missing 󰑌

--- srcs/client/features/events/components/EventDetailContent/EventDetailContent.tsx
+++ srcs/client/features/events/components/EventDetailContent/EventDetailContent.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import type { Room } from '@type';
+import { DialogBody } from '@/components/ui/Dialog';
 import { EventDetailAllergySection } from './EventDetailAllergySection';
 import { EventDetailParticipantsSection } from './EventDetailParticipantsSection';
 import { EventDetailSummarySection } from './EventDetailSummarySection';
