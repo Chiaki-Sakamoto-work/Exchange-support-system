aea2b8264802cfadbd5466cc69922d3f7fc0a5c1
pisyaru <jinyeong1114@gmail.com>
2026-05-27 11:20:05 +0900

    feat: top panel logo

--- srcs/client/app/(main)/layout.tsx
+++ srcs/client/app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { BottomNav } from '@components/layout/BottomNav';
+import { Header } from '@/components/layout/Header';
 
 export default function MainLayout({
   children,
@@ -7,7 +8,7 @@ export default function MainLayout({
 }) {
   return (
     <div className='flex h-dvh flex-col overflow-hidden bg-background'>
-      <header className='flex h-26 shrink-0 items-center gap-2 p-6'></header>
+      <Header />
 
       <main className='mx-auto flex min-h-0 w-full max-w-[632px] flex-1 flex-col pb-24'>
         {children}
